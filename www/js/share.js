1d635bb607e4bbe368688fbc3e5b462f2aae19b1
abhishekvaidyanath <abhishekvaidyanath@gmail.com>
2017-03-06 14:59:15 +0530

    Update share.js

--- www/js/share.js
+++ www/js/share.js
@@ -1,11 +1,12 @@
 function fbShare(){
 	window.plugins.socialsharing.shareViaFacebook('Message via Facebook', 
                                                  null /* img */, 
-                                                function getPhoto(source) {
-      // Retrieve image file location from specified source
+                                                function capturePhoto() {
+      // Take picture using device camera and retrieve image as base64-encoded string
       navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
         destinationType: destinationType.FILE_URI,
-        sourceType: source });
+		saveToPhotoAlbum : true,
+		});
     }, 
                                                  null, 
                                                  function(errormsg){alert("Error: Cannot Share")}
@@ -15,11 +16,12 @@ function fbShare(){
 function whatsappShare(){
 	window.plugins.socialsharing.shareViaWhatsApp('Message via WhatsApp',
                                                  null /* img */,
-                                                function getPhoto(source) {
-      // Retrieve image file location from specified source
+                                               function capturePhoto() {
+      // Take picture using device camera and retrieve image as base64-encoded string
       navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
         destinationType: destinationType.FILE_URI,
-        sourceType: source });
+		saveToPhotoAlbum : true,
+		});
     }, 
                                                  null, 
                                                  function(errormsg){alert("Error: Cannot Share")}
@@ -29,11 +31,12 @@ function whatsappShare(){
 function twitterShare(){
 	 window.plugins.socialsharing.shareViaTwitter('Message via Twitter',
                                                  null /* img */, 
-                                                function getPhoto(source) {
-      // Retrieve image file location from specified source
+                                               function capturePhoto() {
+      // Take picture using device camera and retrieve image as base64-encoded string
       navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
         destinationType: destinationType.FILE_URI,
-        sourceType: source });
+		saveToPhotoAlbum : true,
+		});
     }, 
                                                  null, 
                                                  function(errormsg){alert("Error: Cannot Share")}
